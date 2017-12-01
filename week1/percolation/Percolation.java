e967a3d72061b70fa4c19febdb4ffa8dd60073d2
Jordan Epps <jordan.k.epps@gmail.com>
2017-12-01 15:34:54 -0800

    Percolation - final submission

--- week1/percolation/Percolation.java
+++ week1/percolation/Percolation.java
@@ -1,23 +1,22 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import java.lang.IllegalArgumentException;
 
 public class Percolation {
-  private int[][] grid;
+  private boolean[][] grid;
   private WeightedQuickUnionUF uf;
   private int totalNodes;
   private int totalOpenSites;
   private int gridDimension;
 
-  // sets private variables 
-  public Percolation (int n) {
+  // sets private variables
+  public Percolation(int n) {
     if (n <= 0) throw new IllegalArgumentException("Invalid dimension");
     gridDimension = n;
     totalNodes = (n * n) + 2;
     totalOpenSites = 0;
-    grid = new int[n][n];
+    grid = new boolean[n][n];
     uf = new WeightedQuickUnionUF(totalNodes);
     for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) grid[i][j] = 0;
+      for (int j = 0; j < n; j++) grid[i][j] = false;
     }
   }
 
@@ -25,29 +24,27 @@ public class Percolation {
   // connects it to any surrounding sites that are open
   public void open(int row, int col) {
     checkArgs(row, col);
-    if(!isOpen(row, col)) totalOpenSites++;
-    grid[row-1][col-1] = 1;
+    if (!isOpen(row, col)) totalOpenSites++;
+    grid[row-1][col-1] = true;
     int i = computeIndex(row, col);
     // if location is in the first row, connect the node to the top virtual node
     if (row == 1) uf.union(0, i);
-    // else if the location is in the last row, connect the node to the bottom virtual node
-    else if (row == gridDimension) uf.union(totalNodes - 1, i);
-    else {
-      // connect to above node if open
-      if (isOpen(row + 1, col)) uf.union(i, computeIndex(row + 1, col));
-      // connect to below node if open
-      if (isOpen(row - 1, col)) uf.union(i, computeIndex(row - 1, col));
-    }
+    // if the location is in the last row, connect the node to the bottom virtual node
+    if (row == gridDimension) uf.union(totalNodes - 1, i);
     // connect to the right node if open
     if (col < gridDimension && isOpen(row, col + 1)) uf.union(i, computeIndex(row, col + 1));
     // connect to the left node if open
     if (col > 1 && isOpen(row, col - 1)) uf.union(i, computeIndex(row, col - 1));
+    // connect to above node if open
+    if (row < gridDimension && isOpen(row + 1, col)) uf.union(i, computeIndex(row + 1, col));
+    // connect to below node if open
+    if (row > 1 && isOpen(row - 1, col)) uf.union(i, computeIndex(row - 1, col));
   }
 
   // checks if the site at the given coordinates is open
   public boolean isOpen(int row, int col) {
     checkArgs(row, col);
-    return grid[row-1][col-1] == 1;
+    return grid[row-1][col-1];
   }
 
   // checks if the site at the given coordinates is full
@@ -77,5 +74,5 @@ public class Percolation {
     if (row < 1 || col < 1 || row > gridDimension || col > gridDimension) throw new IllegalArgumentException("Invalid arguments");
   }
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) { }
 }
--- week1/percolation/PercolationStats.java
+++ week1/percolation/PercolationStats.java
@@ -1,45 +1,51 @@
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
-public class PercolationStats{
+public class PercolationStats {
+  private static final double CONSTANT_95 = 1.96;
   private double[] percolationFractions;
+  private int totalTrials;
+  private double mean;
+  private double stddev;
 
   public PercolationStats(int n, int trials) {
     if (n <= 0 || trials <= 0) throw new IllegalArgumentException("Invalid dimension");
     percolationFractions = new double[trials];
+    totalTrials = trials;
     for (int i = 0; i < trials; i++) {
       Percolation trial = new Percolation(n);
       while (!trial.percolates()) {
         trial.open(StdRandom.uniform(n) + 1, StdRandom.uniform(n) + 1);
       }
-      percolationFractions[i] = (double)trial.numberOfOpenSites() / Math.pow(n, 2);
+      percolationFractions[i] = (double) trial.numberOfOpenSites() / Math.pow(n, 2);
     }
+    mean = StdStats.mean(percolationFractions);
+    stddev = StdStats.stddev(percolationFractions);
   }
 
   public double mean() {
-    return StdStats.mean(percolationFractions);
+    return mean;
   }
 
   public double stddev() {
-    return StdStats.stddev(percolationFractions);
+    return stddev;
   }
 
-  public double confidenceLo(int T) {
-    return mean() - (1.96 * stddev() / Math.sqrt(T));
+  public double confidenceLo() {
+    return mean() - (CONSTANT_95 * stddev() / Math.sqrt(totalTrials));
   }
 
-  public double confidenceHi(int T) {
-    return mean() + (1.96 * stddev() / Math.sqrt(T));
+  public double confidenceHi() {
+    return mean() + (CONSTANT_95 * stddev() / Math.sqrt(totalTrials));
   }
 
   public static void main(String[] args) {
     int n = Integer.parseInt(args[0]);
-    int T = Integer.parseInt(args[1]);
-    PercolationStats stats = new PercolationStats(n, T);
+    int t = Integer.parseInt(args[1]);
+    PercolationStats stats = new PercolationStats(n, t);
     StdOut.println("mean                    = " + stats.mean());
     StdOut.println("stddev                  = " + stats.stddev());
-    StdOut.println("95% confidence interval = [" + stats.confidenceLo(T) + ", " + stats.confidenceHi(T) + "]");
+    StdOut.println("95% confidence interval = [" + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
   }
 }
