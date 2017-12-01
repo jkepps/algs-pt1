21d21805505702d816003c8964cf9cfa19affe67
Jordan Epps <jordan.k.epps@gmail.com>
2017-11-30 16:20:35 -0800

    Percolation tweaks

--- week1/percolation/Percolation.java
+++ week1/percolation/Percolation.java
@@ -17,7 +17,7 @@ public class Percolation {
     uf = new WeightedQuickUnionUF(totalNodes);
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) grid[i][j] = 0;
-    } 
+    }
   }
 
   public void open(int row, int col) {
@@ -25,12 +25,20 @@ public class Percolation {
     if(!isOpen(row, col)) totalOpenSites++;
     grid[row-1][col-1] = 1;
     int i = computeIndex(row, col);
+    // if location is in the first row, connect the node to the top virtual node
     if (row == 1) uf.union(0, i);
-    if (row == gridDimension) uf.union(totalNodes - 1, i);
+    // else if the location is in the last row, connect the node to the bottom virtual node
+    else if (row == gridDimension) uf.union(totalNodes - 1, i);
+    else {
+      // connect to above node if open
+      if (isOpen(row + 1, col)) uf.union(i, computeIndex(row + 1, col));
+      // connect to below node if open
+      if (isOpen(row - 1, col)) uf.union(i, computeIndex(row - 1, col));
+    }
+    // connect to the right node if open
     if (col < gridDimension && isOpen(row, col + 1)) uf.union(i, computeIndex(row, col + 1));
+    // connect to the left node if open
     if (col > 1 && isOpen(row, col - 1)) uf.union(i, computeIndex(row, col - 1));
-    if (row < gridDimension && isOpen(row + 1, col)) uf.union(i, computeIndex(row + 1, col));
-    if (row > 1 && isOpen(row - 1, col)) uf.union(i, computeIndex(row - 1, col));
   }
 
   public boolean isOpen(int row, int col) {
@@ -52,7 +60,7 @@ public class Percolation {
     return uf.connected(0, totalNodes - 1);
   }
 
-  // Converts the 2D coordinates of the grid to the 1D index of 
+  // Converts the 2D coordinates of the grid to the 1D index of
   // the union-find object
   private int computeIndex(int row, int col) {
     return ((row - 1) * gridDimension) + col;
